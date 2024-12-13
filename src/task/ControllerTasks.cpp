6b551ccead6aacc9a0374de2a4dae2809a666369
valdisdot <valdisdot@gmail.com>
2024-12-13 19:08:18 +0200

    create and imlement classes PortHandlerStatefulCollectorTask and DataExchangerSaverTask

---
+++ src/task/ControllerTasks.cpp
@@ -0,0 +1,32 @@
+#include "task/ControllerTasks.h"
+
+PortHandlerStatefulCollectorTask::PortHandlerStatefulCollectorTask(const PortHandler& portHandler, const TaskConstants& constants)
+    : Task(
+          "stateful_collector_task",
+          0,
+          constants.PORT_HANDLER_STATEFUL_COLLECTOR_TASK_DELAY,
+          constants.TASK_TYPE_CYCLIC,
+          constants.PORT_HANDLER_STATEFUL_COLLECTOR_TASK_STACK_SIZE,
+          constants.PORT_HANDLER_STATEFUL_COLLECTOR_TASK_PRIORITY,
+          constants),
+      portHandler(portHandler) {}
+
+void PortHandlerStatefulCollectorTask::execute() {
+    portHandler.collectStatefulData();
+}
+
+DataExchangerSaverTask::DataExchangerSaverTask(const InternalStorage& storage, const TaskConstants& constants) 
+    : Task(
+          "data_exchanger_saver_task",
+          0,
+          constants.INTERNAL_STORAGE_TASK_DELAY,
+          constants.TASK_TYPE_CYCLIC,
+          constants.INTERNAL_STORAGE_TASK_STACK_SIZE,
+          constants.INTERNAL_STORAGE_TASK_PRIORITY,
+          constants),
+      storage(storage) {}
+
+void DataExchangerSaverTask::execute() {
+    storage.saveModes();
+    storage.saveState();
+}
