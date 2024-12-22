8e5777b69afd5abaefa75b7f03c2cdf7ff60c3ac
valdisdot <valdisdot@gmail.com>
2024-12-22 10:59:30 +0200

    update JsonSchema
    - remove baudRate proterty (serial connection will have a fixed speed rate)
    - make json names in snake_case
    - add names for message and response jsons

--- src/constant/JsonSchema.h
+++ src/constant/JsonSchema.h
@@ -20,10 +20,9 @@
 struct JsonSchema {
     const char* CONFIG_WORD = "configuration";               /**< Represents the configuration field name. */
     const char* MODE_WORD = "mode";                          /**< Represents the mode field name. */
-    const char* BAUD_RATE_WORD = "baudRate";                 /**< Represents the baud rate field name. */
     const char* SSID_WORD = "ssid";                          /**< Represents the SSID field name. */
     const char* PASSWORD_WORD = "password";                  /**< Represents the password field name. */
-    const char* TRANSMISSION_INTERVAL_WORD = "transmissionInterval"; /**< Represents the transmission interval field name. */
+    const char* TRANSMISSION_INTERVAL_WORD = "transmission_interval"; /**< Represents the transmission interval field name. */
 
     const char* MODES_WORD = "modes";                        /**< Represents the modes field name. */
     const char* STATE_WORD = "state";                        /**< Represents the state field name. */
@@ -32,19 +31,26 @@ struct JsonSchema {
     const char* ID_WORD = "id";                              /**< Represents the ID field name. */
 
     const char* CONTROLLER_WORD = "controller";              /**< Represents the controller field name. */
-    const char* HEAP_SIZE_WORD = "heapSize";                 /**< Represents the heap size field name. */
-    const char* HEAP_MIN_SIZE_WORD = "heapMinSize";          /**< Represents the minimum heap size field name. */
-    const char* RESET_REASON_WORD = "resetReasonCode";       /**< Represents the reset reason code field name. */
-    const char* FLASH_SIZE_WORD = "flashMemorySize";         /**< Represents the flash memory size field name. */
+    const char* HEAP_SIZE_WORD = "heap_size";                 /**< Represents the heap size field name. */
+    const char* HEAP_MIN_SIZE_WORD = "heap_min_size";          /**< Represents the minimum heap size field name. */
+    const char* RESET_REASON_WORD = "reset_reason";       /**< Represents the reset reason code field name. */
+    const char* FLASH_SIZE_WORD = "flash_memory_size";         /**< Represents the flash memory size field name. */
     const char* CORES_WORD = "cores";                        /**< Represents the cores field name. */
     const char* REVISION_WORD = "revision";                  /**< Represents the revision field name. */
     const char* FEATURES_WORD = "features";                  /**< Represents the features field name. */
     const char* WIFI_WORD = "wifi";                          /**< Represents the Wi-Fi field name. */
     const char* BLUETOOTH_WORD = "bluetooth";                /**< Represents the Bluetooth field name. */
-    const char* BLUETOOTH_LE_WORD = "bluetoothLE";           /**< Represents the Bluetooth Low Energy field name. */
+    const char* BLUETOOTH_LE_WORD = "bluetooth_le";           /**< Represents the Bluetooth Low Energy field name. */
 
-    const char* MESSAGE_HASH_WORD = "messageHash";           /**< Represents the message hash field name. */
-    const char* MESSAGE_RESPONSE = "response";               /**< Represents the response message field name. */
+    const char* MESSAGE_ID_WORD = "message_id";           /**< Represents the message hash field name. */
+    const char* REQUEST_WORD = "request";           /**< Represents the message hash field name. */
+    const char* NAME_WORD = "name";           /**< Represents the message hash field name. */
+    const char* VALUE_WORD = "value";           /**< Represents the message hash field name. */
+    const char* MESSAGE_HASH_WORD = "message_hash";           /**< Represents the message hash field name. */
+    const char* RESPONSE_WORD = "response";           /**< Represents the message hash field name. */
+    const char* RESULT_WORD = "result";           /**< Represents the message hash field name. */
+    const char* TYPE_WORD = "type";               /**< Represents the response message field name. */
+    const char* DETAILS_WORD = "details";               /**< Represents the response message field name. */
 
     JsonSchema() = default; /**< Default constructor for the JsonSchema structure. */
 };
