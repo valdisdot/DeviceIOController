c36c9779f3d67a13b84bdc5db3ede058d11dc732
valdisdot <valdisdot@gmail.com>
2024-12-22 12:07:40 +0200

    update SerialClient
    - implement changes in the BaseClient
    - create private method 'send' to simply send text into the serial, make the method add '\n#END#\n' after sending the message for simplifying the processing on the serial-user side

--- src/client/SerialClient.cpp
+++ src/client/SerialClient.cpp
@@ -1,41 +1,48 @@
 #include "client/SerialClient.h"
 
+// when esp32 send gibberish stuff to the serial, simply check every T in the stream of [T]\n#END#\n[T]\n#END#\n[T]n#END#\n
+void SerialClient::send(const char *data) {
+    serial.write(data);
+    serial.write(DIVIDER);
+    serial.flush();
+}
+
+void SerialClient::sendState(const char *state) {
+    send(state);
+}
+
+void SerialClient::sendControllerState(const char *controllerState) {
+    send(controllerState);
+}
+
+void SerialClient::sendResponse(const char *response) {
+    send(response);
+}
+
 SerialClient::SerialClient(JsonDocument *dataExchangeJson, const JsonSchema &jsonSchema, InternalStorage &storage, ControllerHandler &controllerHandler, PortHandler &portHandler, HardwareSerial &serial)
     : BaseClient(dataExchangeJson, jsonSchema, storage, controllerHandler, portHandler), serial(serial) {}
-
-// Send responses via serial
-void SerialClient::pushPullingResponce(const char *responseJson) {
-    serial.print(responseJson);
-}
-
-// Push the state via Serial
-void SerialClient::pushMessage() {
-    char buffer[512];
-    serializeJsonPretty((*dataExchangeJson)[jsonSchema.STATE_WORD], buffer, 512);
-    serial.print(buffer);
-}
 
 bool SerialClient::initialize() {
     storage.readConfiguration();
     serial.begin(38400);
     unsigned long start = millis();
-    while (!serial && millis() - start < 1000) { /*wait till ready and 2 seconds*/
+    while (!serial && millis() - start < 1000) { /*wait untill ready plus 2 seconds*/
     }
     while (serial.available() > 0) {
-        serial.read();  // Clear any garbage data
+        serial.read();  //clear any garbage data
     }
     return true;
 }
 
-void SerialClient::run() {
+void SerialClient::step() {
     if (serial.available() > 0) {
-        int length = 256;
+        int length = 512;
         char buffer[length];
         int i = 0;
         while (serial.available() > 0 && i < length) {
             buffer[i++] = (char) serial.read();
         }
         buffer[i > length ? i - 1 : i] = '\0';
-        pullMessageFrom(buffer, true);
+        processMessage(buffer, true);
     }
 }
