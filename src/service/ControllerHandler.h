4728cf846a65eea2af0b0ac83501e7cc0fc29043
valdisdot <valdisdot@gmail.com>
2024-12-29 21:06:05 +0200

    update ControllerHandler
    - refactor to the service directory
    - implemement usage ControllerState as data object instead of sharing JsonDocument

---
+++ src/service/ControllerHandler.h
@@ -0,0 +1,20 @@
+#ifndef CONTROLLER_HANDLER_H
+#define CONTROLLER_HANDLER_H
+
+#include "system/MicroSystem.h"
+#include "capsule/ControllerState.h"
+
+class ControllerHandler {
+   private:
+    ControllerState& state;
+    char id[32];
+
+   public:
+    ControllerHandler(ControllerState& state);
+    const char* getId();
+    void pushControllerState();
+    void reboot();
+    ControllerState& getControllerState();
+};
+
+#endif
