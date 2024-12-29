7926e538bbf0fa928fe07b8107508b4ac0d21576
valdisdot <valdisdot@gmail.com>
2024-12-29 21:13:50 +0200

    update ControllerTasks
    - remove unused constants
    - implement changes in BaseClient, InternalStorage, PortHandler

--- src/task/ControllerTasks.h
+++ src/task/ControllerTasks.h
@@ -1,67 +1,27 @@
-/**
- * @file ControllerTasks.h
- * @brief Defines specialized tasks for handling port state collection and data saving operations.
- *
- * This header file provides concrete implementations of the `Task` class
- * for managing stateful port data collection and saving data to internal storage.
- */
-
 #ifndef CONTROLLER_TASKS_H
 #define CONTROLLER_TASKS_H
 
-#include "client/BaseClient.h"
-#include "port/PortHandler.h"
-#include "storage/InternalStorage.h"
+#include "system/MicroSystem.h"
 #include "task/Task.h"
-#include "constant/TaskConstants.h"
+#include "client/BaseClient.h"
+#include "service/PortHandler.h"
+#include "service/InternalStorage.h"
 
-/**
- * @class PortHandlerStatefulCollectorTask
- * @brief Collects stateful data from the port handler periodically.
- *
- * This task is configured as a cyclic task and runs at a predefined interval,
- * collecting stateful data using the provided `PortHandler` instance.
- */
 class PortHandlerStatefulCollectorTask : public Task {
    private:
-    PortHandler portHandler; /**< Reference to the port handler for data collection. */
+    PortHandler& portHandler; 
 
    public:
-    /**
-     * @brief Constructs a `PortHandlerStatefulCollectorTask` object.
-     * @param portHandler Reference to the `PortHandler` object used for data collection.
-     * @param constants Reference to `TaskConstants` for configuration.
-     */
-    PortHandlerStatefulCollectorTask(const PortHandler& portHandler, const TaskConstants& constants);
-
-    /**
-     * @brief Executes the task to collect stateful data from the port handler.
-     */
+    PortHandlerStatefulCollectorTask(PortHandler& portHandler);
     void execute() override;
 };
 
-/**
- * @class DataExchangerSaverTask
- * @brief Saves data modes and states periodically to internal storage.
- *
- * This task is configured as a cyclic task and runs at a predefined interval,
- * saving data using the provided `InternalStorage` instance.
- */
-class DataExchangerSaverTask : public Task {
+class BackupTask : public Task {
    private:
-    InternalStorage storage; /**< Reference to the internal storage for saving data. */
+    InternalStorage& storage; 
 
    public:
-    /**
-     * @brief Constructs a `DataExchangerSaverTask` object.
-     * @param storage Reference to the `InternalStorage` object for saving data.
-     * @param constants Reference to `TaskConstants` for configuration.
-     */
-    DataExchangerSaverTask(const InternalStorage& storage, const TaskConstants& constants);
-
-    /**
-     * @brief Executes the task to save modes and state to internal storage.
-     */
+    BackupTask(InternalStorage& storage);
     void execute() override;
 };
 
@@ -70,7 +30,7 @@ class ClientRunnerTask : public Task {
     BaseClient& client;
 
    public:
-    ClientRunnerTask(BaseClient& client, const TaskConstants& constants);
+    ClientRunnerTask(BaseClient& client);
 
     void execute() override;
 };
