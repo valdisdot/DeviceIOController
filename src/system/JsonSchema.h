92f285b879764e37eb4c28bc57ce22d2a4586a23
valdisdot <valdisdot@gmail.com>
2025-01-03 00:05:25 +0200

    update JsonSchema: rebuild the architecture
    - make variable names in $-separated style (GCC allows this)
    - make struct-object initialization in this header file

--- src/system/JsonSchema.h
+++ src/system/JsonSchema.h
@@ -1,42 +1,9 @@
 #ifndef JSON_SCHEMA_H
 #define JSON_SCHEMA_H
 
-struct BaseSchema {
-    const char* ID = "id";
-    const char* STATE = "state";
-    const char* CONFIGURATION = "configuration";
-    const char* CONTROLLER = "controller";
-
-    BaseSchema() = default;
-};
-
 struct StateSchema {
     const char* MODES = "modes";
     const char* DATA = "data";
-};
-
-struct ConnectionConfigurationSchema {
-    const char* ROOT = "network";
-    const char* MODE = "mode";
-    const char* SSID = "ssid";
-    const char* PASSWORD = "password";
-    const char* APN_NAME = "apn_name";
-    const char* APN_USER = "apn_user";
-    const char* APN_PASSWORD = "apn_password";
-    const char* TRANSMISSION_INTERVAL = "transmission_interval";
-
-    ConnectionConfigurationSchema() = default;
-};
-
-struct ServerConfigurationSchema {
-    const char* ROOT = "server";
-    const char* ADDRESS = "address";
-    const char* PORT = "port";
-    const char* USER = "user";
-    const char* PASSWORD = "password";
-    const char* CERTIFICATE = "certificate";
-
-    ServerConfigurationSchema() = default;
 };
 
 struct ControllerStateSchema {
@@ -45,34 +12,59 @@ struct ControllerStateSchema {
     const char* STATE$CELLULAR = "cellular";
     const char* STATE$SERIAL = "serial";
     const char* MEMORY = "memory";
-    const char* MEMORY$MIN = "min";
-    const char* MEMORY$MAX = "max";
     const char* REBOOT_REASON = "reboot_reason";
+};
 
-    ControllerStateSchema() = default;
+struct NetworkConfigurationSchema {
+    const char* MODE = "mode";
+    const char* SSID = "ssid";
+    const char* PASSWORD = "password";
+    const char* APN_NAME = "apn_name";
+    const char* APN_USER = "apn_user";
+    const char* APN_PASSWORD = "apn_password";
+};
+
+struct ServerConfigurationSchema {
+    const char* ADDRESS = "address";
+    const char* PORT = "port";
+    const char* USER = "user";
+    const char* PASSWORD = "password";
+    const char* CERTIFICATE = "certificate";
+};
+
+struct ControllerConfigurationSchema {
+    const char* ROOT = "controller";
+    const char* LOG_LEVEL = "log_level";
+    const char* TRANSMISSION_INTERVAL = "transmission_interval";
 };
 
 struct MessageSchema {
-    const char* MESSAGE_ID = "message_id";     
+    const char* TIMESTAMP = "timestamp";
+    const char* ID = "id";
+    const char* MESSAGE_ID = "message_id";
     const char* MESSAGE_HASH = "message_hash";
-    const char* RESPONSE = "response";        
-    const char* REQUEST = "request";           
-    const char* NAME = "name";                 
-    const char* VALUE = "value";              
-    const char* RESULT = "result";          
-    const char* TYPE = "type";              
-    const char* DETAILS = "details";       
-
-    MessageSchema() = default;
+    const char* RESPONSE = "response";
+    const char* REQUEST = "request";
+    const char* NAME = "name";
+    const char* VALUE = "value";
+    const char* RESULT = "result";
+    const char* TYPE = "type";
+    const char* DETAILS = "details";
 };
 
 struct LogSchema {
-    const char * TIMESTAMP = "timestamp";
-    const char * THEME = "theme";
-    const char* INTEGER = "integer";
-    const char* TEXT = "text";
-    const char* OBJECT = "object";
     const char* LEVEL = "level";
+    const char* MODULE = "module";
+    const char* MESSAGE = "message";
+    const char* CAUSE = "cause";
 };
+
+const static StateSchema JSON$STATE;
+const static ControllerStateSchema JSON$CONTROLLER_STATE;
+const static NetworkConfigurationSchema JSON$NETWORK_CONFIGURATION;
+const static ServerConfigurationSchema JSON$SERVER_CONFIGURATION;
+const static ControllerConfigurationSchema JSON$CONTROLLER_CONFIGURATION;
+const static MessageSchema JSON$MESSAGE;
+const static LogSchema JSON$LOG;
 
 #endif
