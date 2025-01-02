9f8378d4250601fca13ab27b271cf0937e8dc2fa
valdisdot <valdisdot@gmail.com>
2025-01-03 00:03:40 +0200

    update MicroSystem.h
    - remove static variables as long as the Constant.h holds them

--- src/system/MicroSystem.h
+++ src/system/MicroSystem.h
@@ -6,21 +6,6 @@
 #include "system/JsonSchema.h"
 #include "system/Constant.h"
 
-const static bool DEBUG_MODE = true;
-
-const static SystemConstant CONSTANT;
-const static TaskConstant TASK_CONSTANT;
-const static MqttConstant MQTT_CONSTANT;
-const static BaseSchema SCHEMA_BASE;
-const static StateSchema SCHEMA_STATE;
-const static ConnectionConfigurationSchema SCHEMA_CONNECTION_CONFIGURATION;
-const static ServerConfigurationSchema SCHEMA_SERVER_CONFIGURATION;
-const static ControllerStateSchema SCHEMA_CONTROLLER_STATE;
-const static LogSchema LOG_SCHEMA;
-const static RequestType REQUEST_TYPE;
-const static MessageSchema SCHEMA_MESSAGE;
-const static LogConstant LOG_CONSTANT;
-
 void copyString(char *destination, const char *source, int limit);
 void concatenateString(char* destination, const char* source, int limit);
 void convertToASCIIString(char* destination, const byte* byteSource, int limit);
